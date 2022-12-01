bf474d0b345862776a6a29004839ab08df2cfc61
kenya6565 <ken.australia0605@gmail.com>
2022-12-02 02:58:34 +0900

    create CartState in Atom

--- src/features/Atoms/CartAtom.ts
+++ src/features/Atoms/CartAtom.ts
@@ -1,1 +1,11 @@
 import { atom } from "recoil";
+import cartItems from "../../components/cartItems";
+
+export const cartState = atom<object>({
+  key: "cartState",
+  default: {
+    cartItems: cartItems,
+    amount: 1,
+    total: 0,
+  },
+});
