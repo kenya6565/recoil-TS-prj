8940892542e560ac168ca2ffaf8881f055fdfa71
kenya6565 <ken.australia0605@gmail.com>
2022-12-03 02:31:46 +0900

    fix Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="nav-center">
+      <h3>Recoil Shopping</h3>
+      <div className="nav-container">
+        <div className="amount-container">
+          <p className="total-amount">amount</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
