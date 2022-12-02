69f55002d2aa17af725ea61ab5bcb035be70af9f
kenya6565 <ken.australia0605@gmail.com>
2022-12-03 02:35:37 +0900

    set Navbar and add RecoilRoot

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <main>App.tsx</main>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 }
 
 export default App;
