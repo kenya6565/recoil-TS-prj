a6395d376999c78b06ad0ce29e71fefd955f0de8
kenya6565 <ken.australia0605@gmail.com>
2022-12-05 23:31:44 +0900

    add removeItem()

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { MinusIcon, PlusIcon } from "../heroIcons";
 import { cartItemContent } from "../cartItems";
+import { useRecoilState } from "recoil";
+import { cartItemState } from "../features/Atoms/CartAtom";
+import cartItems from "../cartItems";
 
 const CartItem = (props: cartItemContent) => {
-  const { img, title, price, amount } = props;
+  const { img, id, title, price, amount } = props;
+  const [cartItem, setCartItem] = useRecoilState(cartItemState);
+
+  const removeItem = (id: number) => {
+    cartItem.cartItems.filter((item) => {
+      return id !== item.id
+    });
+  };
 
   return (
     <article className="cart-item">
@@ -11,7 +21,7 @@ const CartItem = (props: cartItemContent) => {
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">{price}円</h4>
-        <button className="remove-btn" onClick={() => console.log("hoge")}>
+        <button className="remove-btn" onClick={() => removeItem(id)}>
           削除
         </button>
         <button className="amount-btn" onClick={() => console.log("hoge")}>
