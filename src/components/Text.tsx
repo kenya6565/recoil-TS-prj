6a77b6ded012abd196589e34a5a365cfdbd630b8
kenya6565 <ken.australia0605@gmail.com>
2022-12-08 03:32:02 +0900

    create index.ts and Text.tsx

---
+++ src/components/Text.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Text = ({
+  as,
+  children,
+}: {
+  as: any;
+  children: React.ReactNode;
+}) => {
+  return <div>Text</div>;
+};
+
+export default Text;
