d3d1f04f12a1cea9e93a699447f4cdd919b5beb8
kenya6565 <ken.australia0605@gmail.com>
2022-12-09 01:31:05 +0900

    use Text component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import CardContainer from "./components/CardContainer";
 import Navbar from "./components/Navbar";
+import { Text } from "./components/Text";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <CardContainer />
+      <Text as="h1">Hello React</Text>
+      <Text as="h2">Good Bye React</Text>
+      <Text as="span">Good Bye React</Text>
+
     </main>
   );
 };
